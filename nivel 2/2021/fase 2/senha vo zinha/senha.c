e90072f7c8db3f0a8bbf390d1b97e3e949acfc05
Zairo Bastos <zairo_vianahd@hotmail.com>
2023-07-22 02:37:51 -0300

    feat: :rocket: Questão senha da Vó Zinha  - Fase 2 / OBI

---
+++ nivel 2/2021/fase 2/senha vo zinha/senha.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <string.h>  // fornece várias funções para manipulação de strings, como strlen, strcpy, strcat, entre outras.
+
+// Função para ordenar uma string.
+void sortString(char str[]) {
+    int n = strlen(str);
+    for (int i = 0; i < n-1; i++) {
+        for (int j = i+1; j < n; j++) {
+            if (str[i] > str[j]) {
+                char temp = str[i];
+                str[i] = str[j];
+                str[j] = temp;
+            }
+        }
+    }
+}
+
+int main() {
+    int n, m, k;
+    // Lê três números inteiros n, m e k separados por espaço.
+    scanf("%d %d %d", &n, &m, &k);
+
+    char s[n+1];
+    // Lê a senha escrita no papel com caracteres borrados representados por '#'.
+    scanf("%s", s);
+
+    char possib[m][k+1];
+    // Loop para ler m strings e adicioná-las à matriz possib após ordená-las.
+    for (int i = 0; i < m; i++) {
+        scanf("%s", possib[i]);
+        sortString(possib[i]);
+    }
+
+    int p;
+    // Lê um número inteiro p.
+    scanf("%d", &p);
+
+    // Subtrai 1 de p (para ajustar o índice para a base 0).
+    p--;
+
+    // Inicializa a variável j com m - 1, representando o índice do último elemento da matriz possib.
+    int j = m - 1;
+
+    // Loop para percorrer a string s de trás para frente (da direita para a esquerda).
+    for (int i = n - 1; i >= 0; i--) {
+        if (s[i] == '#') {  // Se o caractere atual em s for '#':
+            // Substitui o caractere '#' na posição i por possib[j][p % k], selecionando a letra da matriz de possibilidades de acordo com o valor de p e k.
+            s[i] = possib[j][p % k];
+            // Divide p por k (usando divisão inteira) para avançar para o próximo caractere em possib[j].
+            p /= k;
+            // Decrementa j para ir para a próxima string em possib.
+            j--;
+        }
+    }
+
+    // Imprime a senha descriptografada após as substituições.
+    printf("%s\n", s);
+
+    return 0;
+}
